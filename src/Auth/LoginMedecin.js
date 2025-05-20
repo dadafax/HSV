fd84c4ad39f22defd6f81841a087a2331163958e
dadafax <158064411+dadafax@users.noreply.github.com>
2025-05-20 09:35:20 +0200

    sdf

--- src/Auth/LoginMedecin.js
+++ src/Auth/LoginMedecin.js
@@ -2,34 +2,10 @@ import React, { useState } from "react";
 import axios from "axios";
 import { Button } from "../components/Button";
 
-<<<<<<< Updated upstream:src/Auth/LoginMedecin.js
 const LoginMedecin = () => {
     return (
         <form>
             <h1>Page de connexion du Medecin</h1>
-=======
-const Login = () => {
-    const [login, setLogin] = useState("");
-    const [password, setPassword] = useState("");
-
-    const handleSubmit = async (e) => {
-        e.preventDefault();
-        try {
-            const response = await axios.post("http://localhost:5000/api/login", {
-                login,
-                password
-            });
-            console.log("✅ Connexion réussie :", response.data);
-            // Tu peux stocker l'utilisateur ou un token ici
-        } catch (error) {
-            console.error("❌ Échec de la connexion :", error.response?.data || error.message);
-            alert("Identifiants invalides.");
-        }
-    };
-
-    return (
-        <form onSubmit={handleSubmit}>
->>>>>>> Stashed changes:src/Auth/Login.js
             <div className="group">
                 <label htmlFor="login">Adresse mail</label>
                 <input
@@ -57,8 +33,4 @@ const Login = () => {
     );
 };
 
-<<<<<<< Updated upstream:src/Auth/LoginMedecin.js
 export default LoginMedecin
-=======
-export default Login;
->>>>>>> Stashed changes:src/Auth/Login.js
