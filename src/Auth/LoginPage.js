c4e33a97468015bbf56780d858e8c02eac2d70a1
Tankiste07 <remi007tank@gmail.com>
2025-05-20 09:19:19 +0200

    pageconnextion

---
+++ src/Auth/LoginPage.js
@@ -0,0 +1,24 @@
+import React from "react";
+import { Form } from "react-router-dom";
+import { Button } from "../components/Button";
+
+const LoginPage = () => {
+    return (
+        <form>
+            <h1>Page de connexion</h1>
+            <div className="group">
+                <label htmlFor="login" ></label>
+                <input type="text" name="login" placeholder="adresse mail"/>
+            </div>
+            <div clasName="group">
+                <label htmlFor="password" ></label>
+                <input type="text" name="password" placeholder="Mot de passe"/>
+            </div>
+            <div className="group">
+                <Button page="../Auth/LoginPage" text="Connexion"></Button>
+            </div>
+        </form>
+    )
+}
+
+export default LoginPage
