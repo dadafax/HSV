131c8612b41fccb85cbd3ba7004b9212a1a3842a
Tankiste07 <remi007tank@gmail.com>
2025-05-20 09:39:12 +0200

    inscription page

--- src/Auth/LoginPage.js
+++ src/Auth/LoginPage.js
@@ -5,7 +5,7 @@ import { Button } from "../components/Button";
 const LoginPage = () => {
     return (
         <form>
-            <h1>Page de connexion</h1>
+            <h1>Page d'inscription</h1>
             <div className="group">
                 <label htmlFor="login" ></label>
                 <input type="text" name="login" placeholder="adresse mail"/>
@@ -15,7 +15,7 @@ const LoginPage = () => {
                 <input type="text" name="password" placeholder="Mot de passe"/>
             </div>
             <div className="group">
-                <Button page="../Auth/LoginPage" text="Connexion"></Button>
+                <Button page="../Auth/LoginPage" text="Inscription"></Button>
             </div>
         </form>
     )
