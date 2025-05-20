0f0d54369c40d4576055499c8315d86fead51633
LeoChris88 <leochan2005@outlook.fr>
2025-05-20 13:40:08 +0200

    Test

--- server.js
+++ server.js
@@ -1,21 +1,13 @@
 const express = require('express');
-const cors = require('cors');
 const connectDB = require('./db');
-const authRoutes = require('./src/routes/auth');
 
 const app = express();
+const PORT = 5000;
 
-// Connexion à la base de données
 connectDB();
 
-// Middleware
-app.use(cors());
 app.use(express.json());
 
-// Routes
-app.use('/api/auth', authRoutes);
-
-const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`🚀 Serveur lancé sur le port ${PORT}`);
 });
