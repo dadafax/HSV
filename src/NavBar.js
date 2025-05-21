e124d7d346f9cb41e70724d3d8e8cbdae2251ec4
Tankiste07 <remi007tank@gmail.com>
2025-05-21 09:10:03 +0200

    accueil to deconnexion

--- src/NavBar.js
+++ src/NavBar.js
@@ -5,7 +5,7 @@ const NavBar = () => {
     return (
         <nav>
             <ul>
-                <li><Link to="/">Accueil</Link></li>
+                <li><Link to="/">Déconnexion</Link></li>
                 <li><Link to="/book">Réserver</Link></li>
                 <li><Link to="/dashboard">Tableau de bord</Link></li>
             </ul>
