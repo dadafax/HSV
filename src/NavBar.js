3257bce3c5b251b665bfea827a997ac6e59857bf
Tankiste07 <remi007tank@gmail.com>
2025-05-21 09:17:21 +0200

    navbar lien

--- src/NavBar.js
+++ src/NavBar.js
@@ -1,13 +1,15 @@
 import React from "react";
 import { Link } from 'react-router-dom';
+import DashboardPage from "./pages/DashboardPage";
+import BookPage from "./pages/BookPage";
 
 const NavBar = () => {
     return (
         <nav>
             <ul>
                 <li><Link to="/">Déconnexion</Link></li>
-                <li><Link to="/book">Réserver</Link></li>
-                <li><Link to="/dashboard">Tableau de bord</Link></li>
+                <li><Link to="./pages/BookPage">Réserver</Link></li>
+                <li><Link to="/DashboardPage">Tableau de bord</Link></li>
             </ul>
         </nav>
     );
