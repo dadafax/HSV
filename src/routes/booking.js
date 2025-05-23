554ce2071fc6da6c8ae42ddeb6254cc2d395ebb7
dadafax <158064411+dadafax@users.noreply.github.com>
2025-05-23 09:16:08 +0200

    fix booking

--- src/routes/booking.js
+++ src/routes/booking.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const Booking = require('./models/Booking');
+const Booking = require('../models/Booking');
 
 // Route pour créer une réservation
 router.post('/', async (req, res) => {
