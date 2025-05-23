7438fb30f086a55c98de2732eb8f3d3c9db4cd8b
dadafax <158064411+dadafax@users.noreply.github.com>
2025-05-23 09:51:54 +0200

    Update db.js

--- db.js
+++ db.js
@@ -4,10 +4,7 @@ require('dotenv').config({ path: path.join(__dirname, 'server', '.env') });
 
 const connectDB = async () => {
   try {
-    await mongoose.connect('mongodb+srv://Leo:1234@cluster0.rfsaemr.mongodb.net/', {
-      useNewUrlParser: true,
-      useUnifiedTopology: true
-    });
+    await mongoose.connect('mongodb+srv://Leo:1234@cluster0.rfsaemr.mongodb.net/');
     console.log('✅ MongoDB EST BIENNN connecté');
   } catch (error) {
     console.error('❌ Erreur de connexion MongoDB :', error.message);
